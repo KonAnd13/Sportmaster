2450d9f7b77bcd6b02ef55fc2ad73d006d6e61b5
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-09-19 15:22:07 +0300

    Update Sportmaster.java

--- src/Sportmaster.java
+++ src/Sportmaster.java
@@ -6,8 +6,9 @@ public class Sportmaster {
         int amountOfPastPurchase = 5000;
         int amountOfCurrentPurchase = 14000;
         int bonus;
+        final int AmountForBonus = 1000; //The buyer receives bonuses for every 1000 rubles in the amount of the check.
 
-        //Виды бонусных карт
+        //Types of Bonus Cards
         final int STANDART_LEVEL = 15_000;
         final int BONUS_STANDART = 50;
 
@@ -17,17 +18,17 @@ public class Sportmaster {
         final int GOLD_LEVEL = 150_000;
         final int BONUS_GOLD = 100;
 
-        //Сумма всех покупок
+        //The sum of all purchases
         int amountOfAllPurchase = amountOfPastPurchase + amountOfCurrentPurchase;
 
         if (amountOfAllPurchase <= STANDART_LEVEL) {
-            bonus = amountOfCurrentPurchase / 1000 * BONUS_STANDART;
+            bonus = amountOfCurrentPurchase / AmountForBonus * BONUS_STANDART;
             System.out.println(bonus);
         } else if (amountOfAllPurchase <= SILVER_LEVEL) {
-            bonus = amountOfCurrentPurchase / 1000 * BONUS_SILVER;
+            bonus = amountOfCurrentPurchase / AmountForBonus * BONUS_SILVER;
             System.out.println(bonus);
         } else if (amountOfAllPurchase > GOLD_LEVEL) {
-            bonus = amountOfCurrentPurchase / 1000 * BONUS_GOLD;
+            bonus = amountOfCurrentPurchase / AmountForBonus * BONUS_GOLD;
             System.out.println(bonus);
         }
     }
