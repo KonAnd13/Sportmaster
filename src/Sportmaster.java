44a20dd59b060025ffa77109369cd33a3f0fe64c
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-09-19 15:05:11 +0300

    Add files via upload

---
+++ src/Sportmaster.java
@@ -0,0 +1,34 @@
+/*
+Created by Andrew Konshin
+*/
+public class Sportmaster {
+    public static void main(String[] args) {
+        int amountOfPastPurchase = 5000;
+        int amountOfCurrentPurchase = 14000;
+        int bonus;
+
+        //Виды бонусных карт
+        final int STANDART_LEVEL = 15_000;
+        final int BONUS_STANDART = 50;
+
+        final int SILVER_LEVEL = 150_000;
+        final int BONUS_SILVER = 70;
+
+        final int GOLD_LEVEL = 150_000;
+        final int BONUS_GOLD = 100;
+
+        //Сумма всех покупок
+        int amountOfAllPurchase = amountOfPastPurchase + amountOfCurrentPurchase;
+
+        if (amountOfAllPurchase <= STANDART_LEVEL) {
+            bonus = amountOfCurrentPurchase / 1000 * BONUS_STANDART;
+            System.out.println(bonus);
+        } else if (amountOfAllPurchase <= SILVER_LEVEL) {
+            bonus = amountOfCurrentPurchase / 1000 * BONUS_SILVER;
+            System.out.println(bonus);
+        } else if (amountOfAllPurchase > GOLD_LEVEL) {
+            bonus = amountOfCurrentPurchase / 1000 * BONUS_GOLD;
+            System.out.println(bonus);
+        }
+    }
+}
